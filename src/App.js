88d1b481a6920ca617f4c10b539489911a6eb4a8
parkhyeonji <iamphj3@naver.com>
2022-08-04 20:03:28 +0900

    feat: Form.js 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <TodoListTemplate form={<Form/>}>
+          템플릿 완성
+        </TodoListTemplate>
       </div>
     );
   }
